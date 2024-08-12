4cadfca6d618a30b80fa4b074c74d6a4964271d9
Mazlum2003 <mkakoev@gmail.com>
2024-08-12 14:40:38 +0300

    удалил в классе UserDaoImpl

--- src/main/java/com/example/demo221/dao/UserDaoImpl.java
+++ src/main/java/com/example/demo221/dao/UserDaoImpl.java
@@ -15,31 +15,26 @@ public class UserDaoImpl implements UserDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional(readOnly = true)
     @Override
     public List<User> findAll() {
         return entityManager.createQuery("from User", User.class).getResultList();
     }
 
-    @Transactional(readOnly = true)
     @Override
     public User findById(Long id) {
         return entityManager.find(User.class, id);
     }
 
-    @Transactional
     @Override
     public void save(User user) {
         entityManager.persist(user);
     }
 
-    @Transactional
     @Override
     public void update(User user) {
         entityManager.merge(user);
     }
 
-    @Transactional
     @Override
     public void delete(Long id) {
         User user = findById(id);
